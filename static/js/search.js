5e6af1f98ec7aa70777850b062c4cd4309a9d853
Ryan Luo <xiaoxuanwangwin102@outlook.com>
2024-09-28 09:51:21 +0800

    none

--- static/js/search.js
+++ static/js/search.js
@@ -2,7 +2,7 @@ function searchInPage() {
     var searchText = prompt("请输入要查找的内容：", "");
     if (searchText) {
       if (window.find) {
-        var found = window.find(searchText, false, false, true, false, false, false);
+        var found = window.find(searchText);
         if (!found) {
           alert("未找到匹配的内容！");
         }
