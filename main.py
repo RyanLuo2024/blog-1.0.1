648fd8d60fde10b258bbdd0257ca0a0fdd2e103e
RyanLuo2024 <xiaoxuanwangwin102@outlook.com>
2025-02-01 19:55:53 +0800

    .

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from logs import create_app, setup_log
 from routes.api import api
 from routes.auth import auth
 from routes.blog import blog
 from routes.others import others
-import time
 import config
 app = Flask(__name__)
-
-app.config['LOG_FILE'] = 'logs/'+time.strftime("%Y_%m_%d_%H.%M.%S")+'.log'
-app.config['LOG_LEVEL'] = 'INFO'
+app = create_app("development", app)
+# app.config['LOG_FILE'] = 'logs/'+time.strftime("%Y_%m_%d_%H.%M.%S")+'.log'
+# app.config['LOG_LEVEL'] = 'INFO'
 app.register_blueprint(api,url_prefix="/")
 app.register_blueprint(auth,url_prefix="/")
 app.register_blueprint(blog,url_prefix='/')
